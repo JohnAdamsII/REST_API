a38f2a1edd089a16a3df39885badbf1abe1b4e9c
Jadams <John Adams>
2022-04-03 18:05:34 -0600

    added delete, db update next

--- src/client.py
+++ src/client.py
@@ -3,6 +3,7 @@ import requests
 BASE_URL = "http://127.0.0.1:5000/"
 PUT_API_PATH = "/video/1"
 GET_API_PATH = "/video/1"
+DELETE_API_PATH = "/video/1"
 
 # PUT request
 response = requests.put(BASE_URL + PUT_API_PATH, {"likes": 10, "name": "my video", "views": 1000})
@@ -16,3 +17,16 @@ response = requests.get(BASE_URL + GET_API_PATH)
 print(f'{response.request=} {response.url=} {response.status_code=} {response.reason=}')
 print(f'{response.json()=}')
 #print(response.json().get('KEY_NAME'))
+
+input()
+
+# DELETE request
+response = requests.delete(BASE_URL + DELETE_API_PATH)
+print(f'{response.request=} {response.url=} {response.status_code=} {response.reason=}')
+print(f'{response}')
+
+input()
+
+response = requests.get(BASE_URL + GET_API_PATH)
+print(f'{response.request=} {response.url=} {response.status_code=} {response.reason=}')
+print(f'{response.json()=}')
--- src/flask_server.py
+++ src/flask_server.py
@@ -13,7 +13,11 @@ videos = {}
 
 def abort_if_video_id_doesnt_exist(video_id):
     if video_id not in videos:
-        abort(404,message="Video is is not valid...")
+        abort(404,message="Video could not be found")
+
+def abort_if_video_id_exist(video_id):
+    if video_id in videos:
+        abort(409,message="Video already exists with given ID")
 
 class Youtube_REST_API(Resource):
     def get(self, video_id: str): #GET = READ data
@@ -21,12 +25,15 @@ class Youtube_REST_API(Resource):
         return videos[video_id] #must be JSON serializable
 
     def put(self, video_id: str): #PUT = Update data
+        abort_if_video_id_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id]= args
         return videos[video_id], 201
     
     def delete(self, video_id):
-        pass
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Youtube_REST_API,"/video/<int:video_id>")
         
